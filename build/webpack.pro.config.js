27d5fd31340d995d0ddf44298f15571c0b0ebd78
shine <xiaoshaoqq@gmial.com>
2018-09-02 17:25:41 +0800

    update webpack production config

--- build/webpack.pro.config.js
+++ build/webpack.pro.config.js
@@ -4,85 +4,89 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-const Config = require('./config');
+const ProConfig = require('./config').pro;
+const fileName = require('./config').fileName;
 
-
-const htmlWebpack = (file) => {
-    let filePath = path.resolve(__dirname, `../resource/views/${file}/index.html`)
-    return new HtmlWebpackPlugin({
-            filename: path.resolve(__dirname, `../dist/views/${file}/index.html`),
-            inject: 'body',
-            template: 'html-withimg-loader!'+ filePath ,
-            chunks: [file],
-            inlineSource: '.(js|css)$'
-    })
+const htmlWebpack = () => {
+    let plugins = [];
+    let filePath = null;
+    for (let file of fileName){
+        plugins.push(
+            new HtmlWebpackPlugin({
+                filename: path.resolve(__dirname, `../dist/views/${file.name}/index.html`),
+                inject: 'body',
+                template: 'html-withimg-loader!'+ path.resolve(__dirname, `../resource/views/${file.name}/index.html`),
+                chunks: [file.name],
+                inlineSource: '.(js|css)$'
+            })
+        )
+    }
+    return plugins;
 }
 
-const entry = {
-    home: path.resolve(__dirname, '../resource/views/home/index.js'),
-    about: path.resolve(__dirname, '../resource/views/about/index.js'),
+const entryObj = () => {
+    let entry = {}
+    for (let file of fileName){
+        if (file.js)
+            entry[file.name] = path.resolve(__dirname, `../resource/views/${file.name}/index.js`);
+    }
+    return entry;
 }
 
-module.exports = {
-    entry: entry,
-    output: {
-        path: path.resolve(__dirname,'../dist'),
-        filename: 'views/[name]/index.[hash:10].js',
-        publicPath: '/',
-    },
-    module: {
-        rules: [
-            //解析.js
-            {   
-                test: '/\.js$/',  
-                loader: 'babel',  
-                exclude: path.resolve(__dirname, 'node_modules'), 
-                include: path.resolve(__dirname, '../resource'),
-                query: {
-                    presets: ['env'] 
+module.exports =  () => {
+
+    let plugins = [
+            new ExtractTextPlugin({
+                filename: 'views/[name]/index.[hash:10].css'
+            }),
+            new CopyWebpackPlugin([
+                { 
+                    from: 'resource/static', 
+                    to: 'static'
                 }
-            },
-            {
-                test: /\.less|css$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', `sass-loader`]
-                }),
-            },
-            { 
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file-loader',
-                query: {
-                    name: 'assets/images/[name]-[hash:5].[ext]'
-                },
-            },
-            {
-                test: /\.(htm|html)$/i,
-                use:[ 'html-withimg-loader'] 
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'views/[name]/index.[hash:10].css'
-        }),
-        htmlWebpack ('home'),
-        htmlWebpack ('about'),
-        new CopyWebpackPlugin([
-            { 
-                from: 'resource/static', 
-                to: 'static'
-            }
-        ])
+            ])
+        ];
+    plugins = plugins.concat(htmlWebpack())
 
-        //设置每一次build之前先删除dist
-        // new CleanWebpackPlugin(
-        //     ['dist/*', 'dist/*',],　     //匹配删除的文件
-        //     {
-        //         root: __dirname,       　　　　　　　　　　//根目录
-        //         verbose: true,        　　　　　　　　　　//开启在控制台输出信息
-        //         dry: false        　　　　　　　　　　//启用删除文件
-        //     }
-        // )
-    ],
+    return {
+        entry: entryObj,
+        output: {
+            path: path.resolve(__dirname,'../dist'),
+            filename: 'views/[name]/index.[hash:10].js',
+            publicPath: '/',
+        },
+        module: {
+            rules: [
+                //解析.js
+                {   
+                    test: '/\.js$/',  
+                    loader: 'babel',  
+                    exclude: path.resolve(__dirname, 'node_modules'), 
+                    include: path.resolve(__dirname, '../resource'),
+                    query: {
+                        presets: ['env'] 
+                    }
+                },
+                {
+                    test: /\.less|css$/,
+                    loader: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader', `sass-loader`]
+                    }),
+                },
+                { 
+                    test: /\.(png|jpg|gif|svg)$/,
+                    loader: 'file-loader',
+                    query: {
+                        name: 'assets/images/[name]-[hash:5].[ext]'
+                    },
+                },
+                {
+                    test: /\.(htm|html)$/i,
+                    use:[ 'html-withimg-loader'] 
+                }
+            ]
+        },
+        plugins: plugins,
+    }
 }
